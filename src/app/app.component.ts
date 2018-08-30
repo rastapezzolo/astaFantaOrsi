e17a1c155a69ebf6d03eb569dc845677a270c760
Luca Langella <rastapezzolo86@gmail.com>
2018-08-30 18:06:09 +0200

    prova finale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,7 +11,7 @@ import { HomePage } from '../pages/home/home';
 export class Asta {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +19,7 @@ export class Asta {
       splashScreen.hide();
     });
   }
+
+
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,71 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   items: Array<{title: string, sum: number, icon: string}>;
   roles: string[];
+  data: Array<{nome:string, prezzo:number}>
+  nome: string[]
+  prezzo: number[]
+  sum: {portieri: number,difensori: number,centrocampisti: number,attaccanti: number}
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private storage: Storage) {
+  	
   	this.roles = ['portieri', 'difensori','centrocampisti','attaccanti']
   	this.items = []
+  	this.nome = []
+  	this.prezzo = []
+  	this.data = []
+  	this.sum = {portieri: 0, difensori: 0, centrocampisti: 0, attaccanti: 0}
+
   	for(let role of this.roles){
-		this.items.push({ title: role, sum: 0, icon: 'football'})
+		this.items.push({ title: role, sum: 0, icon: 'football' })
   	}
+  
   }
 
+  public addPlayer(ruolo: string, i){
+  	
+  	var prezzo = (this.prezzo[i])
+  	var nome = this.nome[i]
+  	if('undefined' !== typeof ruolo && 'undefined' !== typeof nome && 'undefined' !== typeof prezzo){
+	  	this.data.push({ nome, prezzo })
+	  	this.storage.set(`players_${ruolo}`, this.data);
+  	}
+  	this.prezzo[i] = 0
+  	this.nome[i] = ''
+  	this.sum[ruolo] += parseInt(prezzo)
+  
+  }
+
+  public getPlayer(ruolo: string){
+  	
+  	var p = this.storage.get(`players_${ruolo}`)
+
+  	return p
+  
+  }
+
+  public clearPlayers(ruolo: string){
+  
+  	this.storage.remove(`players_${ruolo}`);
+  	this.sum[ruolo] = 0
+
+  }
+
+  public clearAllPlayers(){
+  
+  	this.storage.clear();
+  
+  }
+
+  public getSum(ruolo: string){
+  	
+  	var players = this.getPlayer(ruolo)
+  	var sum = 0
+  	players.then((val)=>{
+	  	for(let player of val){
+	  		sum += parseInt(player.prezzo)
+	  	}
+  		this.sum[ruolo] = sum
+  	})
+  
+  }
 }
